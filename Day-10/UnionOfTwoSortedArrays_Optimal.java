3ea1c68055d264dfb2da8b01a644118971b7e49c
Vanga Sreeja <vsreeja64@gmail.com>
2026-07-22 22:12:34 +0530

    Implement union of two sorted arrays

---
+++ Day-10/UnionOfTwoSortedArrays_Optimal.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int i = 0;
+        int j = 0;
+
+        while (i < a.length && j < b.length) {
+
+            if (a[i] <= b[j]) {
+
+                if (ans.size() == 0 || ans.get(ans.size() - 1) != a[i]) {
+                    ans.add(a[i]);
+                }
+
+                i++;
+
+            } else {
+
+                if (ans.size() == 0 || ans.get(ans.size() - 1) != b[j]) {
+                    ans.add(b[j]);
+                }
+
+                j++;
+            }
+        }
+
+        // Add remaining elements of first array
+        while (i < a.length) {
+
+            if (ans.size() == 0 || ans.get(ans.size() - 1) != a[i]) {
+                ans.add(a[i]);
+            }
+
+            i++;
+        }
+
+        // Add remaining elements of second array
+        while (j < b.length) {
+
+            if (ans.size() == 0 || ans.get(ans.size() - 1) != b[j]) {
+                ans.add(b[j]);
+            }
+
+            j++;
+        }
+
+        return ans;
+    }
+}
